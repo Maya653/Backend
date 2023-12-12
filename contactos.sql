467fb673573ce956db3861a4c7bc997116657aed
Maya653 <122810922+Maya653@users.noreply.github.com>
2023-12-12 07:59:44 +0000

    backend funcional con seguridad

--- contactos.sql
+++ contactos.sql
@@ -1,11 +1,25 @@
-CREATE TABLE contactos (
+-- Crear la tabla de contactos
+CREATE TABLE  IF NOT EXISTS contactos (
     email TEXT PRIMARY KEY,
     nombre TEXT,
     telefono TEXT
 );
 
+-- Insertar datos de ejemplo
 INSERT INTO contactos (email, nombre, telefono)
-VALUES ("juan@example.com", "Juan Pérez", "555-123-4567");
+VALUES ('juan@example.com', 'Juan Pérez', '555-123-4567');
 
 INSERT INTO contactos (email, nombre, telefono)
-VALUES ("maria@example.com", "María García", "555-678-9012");
+VALUES ('maria@example.com', 'María García', '555-678-9012');
+
+
+
+--tabla de usuarios
+CREATE TABLE IF NOT EXISTS usuarios (
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    token VARCHAR(32) UNIQUE,
+    TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO usuarios (username, password, token) VALUES ('usuario@gmail.com', '1234', 'yyyy');
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.staticfiles import StaticFiles
-import sqlite3
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+import sqlite3
+import secrets
+import hashlib
+from fastapi.security import HTTPBasic,HTTPBearer,HTTPBasicCredentials, HTTPAuthorizationCredentials
 
 app = FastAPI()
 
-# Crea la base de datos
-conn = sqlite3.connect("contactos.db")
-c = conn.cursor()
-
-
+# Configuración CORS
 origins = [
     "https://contactos-frontend-6d58a4eb9f51.herokuapp.com",
     "https://contactos-backen-b4d88f351253.herokuapp.com",
@@ -25,13 +24,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-                              
+# Modelos
 class Contacto(BaseModel):
-    email : str
-    nombre : str
-    telefono : str
+    email: str
+    nombre: str
+    telefono: str
 
+class User(BaseModel):
+    username: str
+    password: str
 
+# Funciones de utilidad
+def generate_token():
+    return secrets.token_urlsafe(32)
+
+def hash_password(password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    return hashed_password
+
+# Función para obtener la conexión en cada solicitud
+def get_db():
+    db = sqlite3.connect("contactos.db")
+    try:
+        yield db
+    finally:
+        db.close()
 
 # Rutas para las operaciones CRUD
 
@@ -40,71 +57,139 @@ def root():
     return {"HOLA MUNDO"}
 
 @app.post("/contactos")
-async def crear_contacto(contacto: Contacto):
+def crear_contacto(contacto: Contacto, conn: sqlite3.Connection = Depends(get_db)):
     """Crea un nuevo contacto."""
-    # TODO Inserta el contacto en la base de datos y responde con un mensaje
-    c = conn.cursor()
-    c.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
-              (contacto.email, contacto.nombre, contacto.telefono))
-    conn.commit()
+    # Inserta el contacto en la base de datos y responde con un mensaje
+    with conn:
+        c = conn.cursor()
+        c.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
+                  (contacto.email, contacto.nombre, contacto.telefono))
+        conn.commit()
     return contacto
 
 @app.get("/contactos")
-async def obtener_contactos():
+def obtener_contactos(conn: sqlite3.Connection = Depends(get_db)):
     """Obtiene todos los contactos."""
-    # TODO Consulta todos los contactos de la base de datos y los envia en un JSON
-    c = conn.cursor()
-    c.execute('SELECT * FROM contactos')
-    response = []
-    for row in c:
-        contacto = {"email":row[0], "nombre":row[1], "telefono":row[2]}
-        response.append(contacto)
+    # Consulta todos los contactos de la base de datos y los envía en un JSON
+    with conn:
+        c = conn.cursor()
+        c.execute('SELECT * FROM contactos')
+        response = [{"email": row[0], "nombre": row[1], "telefono": row[2]} for row in c.fetchall()]
     return response
 
 @app.get("/contactos/{email}")
-async def obtener_contacto(email: str):
+def obtener_contacto(email: str, conn: sqlite3.Connection = Depends(get_db)):
     """Obtiene un contacto por su email."""
     # Consulta el contacto por su email
-    c = conn.cursor()
-    c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
-    row = c.fetchone()
+    with conn:
+        c = conn.cursor()
+        c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
+        row = c.fetchone()
     if row:
         contacto = {"email": row[0], "nombre": row[1], "telefono": row[2]}
         return JSONResponse(content=contacto)
     else:
         return JSONResponse(content={}, status_code=404)
 
-from fastapi import HTTPException
-
 @app.put("/contactos/{email}")
-async def actualizar_contacto(email: str, contacto: Contacto):
+def actualizar_contacto(email: str, contacto: Contacto, conn: sqlite3.Connection = Depends(get_db)):
     """Actualiza un contacto."""
     try:
-        c = conn.cursor()
-        c.execute('UPDATE contactos SET nombre = ?, telefono = ? WHERE email = ?',
-                  (contacto.nombre, contacto.telefono, email))
-        conn.commit()
+        with conn:
+            c = conn.cursor()
+            c.execute('UPDATE contactos SET nombre = ?, telefono = ? WHERE email = ?',
+                      (contacto.nombre, contacto.telefono, email))
+            conn.commit()
 
-        if c.rowcount == 0:
-            raise HTTPException(status_code=404, detail="Contacto no encontrado")
-        
+            if c.rowcount == 0:
+                raise HTTPException(status_code=404, detail="Contacto no encontrado")
+
         return contacto
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
-
 @app.delete("/contactos/{email}")
-async def eliminar_contacto(email: str):
+def eliminar_contacto(email: str, conn: sqlite3.Connection = Depends(get_db)):
     """Elimina un contacto."""
-    # TODO Elimina el contacto de la base de datos
-    c = conn.cursor()
-    c.execute('DELETE FROM contactos WHERE email = ?', (email,))
-    conn.commit()
+    # Elimina el contacto de la base de datos
+    with conn:
+        c = conn.cursor()
+        c.execute('DELETE FROM contactos WHERE email = ?', (email,))
+        conn.commit()
     return {"elemento borrado"}
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
+security_basic = HTTPBasic()
+security_bearer = HTTPBearer()
+
+
+@app.get("/root")
+def root(credentials: HTTPAuthorizationCredentials = Depends(security_bearer), conn: sqlite3.Connection = Depends(get_db)):
+    usuario_token = credentials.credentials
+
+    with conn:
+        c = conn.cursor()
+        c.execute("SELECT token FROM usuarios WHERE token = ?", (usuario_token,))
+        result = c.fetchone()
+
+    if result and usuario_token == result[0]:
+        return {"message": "TOKEN válido"}
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="TOKEN no válido")
+
+
+@app.post("/token")
+def login(credentials: HTTPBasicCredentials = Depends(security_basic), conn: sqlite3.Connection = Depends(get_db)):
+    # Verifica qué tipo de credenciales estás utilizando
+    if isinstance(credentials, HTTPBasicCredentials):
+        # Si estás utilizando HTTPBasicCredentials, puedes acceder a 'username' y 'password'
+        username = credentials.username
+        password = credentials.password
+
+        # Realiza la lógica necesaria con el username y password (por ejemplo, autenticación)
+        hashed_password = hash_password(password)
+
+        with conn:
+            c = conn.cursor()
+            c.execute("SELECT token FROM usuarios WHERE username = ? AND password = ?", (username, hashed_password))
+            result = c.fetchone()
+
+    elif isinstance(credentials, HTTPBearer):
+        # Si estás utilizando HTTPBearer, puedes acceder al token con 'credentials.credentials'
+        # Realiza la lógica necesaria con el token
+        token = credentials.credentials
+
+        # Puedes validar el token o realizar otras operaciones según tus necesidades
+
+    else:
+        # Manejo de otro tipo de credenciales si es necesario
+        raise HTTPException(status_code=400, detail="Tipo de credenciales no admitido")
+
+    if result:
+        token = result[0]
+        return {"access_token": token, "token_type": "bearer"}
+    else:
+        raise HTTPException(status_code=401, detail="Usuario o contraseña incorrectos")
+
+
+@app.post("/register")
+def register(user: User, conn: sqlite3.Connection = Depends(get_db)):
+    username = user.username
+    password = user.password
+    
+    token = generate_token()
+    hashed_password = hash_password(password)
+
+    with conn:
+        c = conn.cursor()
+        c.execute("INSERT INTO usuarios (username, password, token) VALUES (?, ?, ?)", (username, hashed_password, token))
+        conn.commit()
+
+    return {"message": "Usuario registrado", "token": token}
